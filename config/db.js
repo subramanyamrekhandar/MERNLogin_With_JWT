e2fe9d47e7da04695500a27d00ada99ed6051849
Subramanyam Rekhandar <53898299+subramanyamrekhandar@users.noreply.github.com>
2024-06-02 09:05:05 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://codingexperthub:NEQ31bvWwHUTeUvi@codingexperthub.y9m0ywe.mongodb.net/test", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
